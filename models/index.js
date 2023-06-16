63867fae23c4c98c405b1b43fa9ccbb7288bbde9
Nico7522 <nico.daddabbo7100#gmail.com>
2023-06-16 16:26:33 +0200

    ee

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require('sequelize');
-const { DB_SERVEUR, DB_DATABASE, DB_USERNAME, DB_PASSWORD } = process.env.production;
+const { DB_SERVEUR, DB_DATABASE, DB_USERNAME, DB_PASSWORD } = process.env;
 
 const dataBase = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
     host: DB_SERVEUR,
