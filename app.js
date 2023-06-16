7ef267003d8db2627b8091f68da93296d9a09359
Nico7522 <nico.daddabbo7100#gmail.com>
2023-06-16 16:36:48 +0200

    dd

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ dataBase.sequelize.authenticate()
 
 
 if (process.env.NODE_ENV === "development") {
-    dataBase.sequelize.sync({alter : { drop: false}});
+    // dataBase.sequelize.sync({alter : { drop: false}});
 }
 appRecipe.use(express.static('public'));
 
