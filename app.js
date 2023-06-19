4bf600bc42c9d954a598e5107c16901c0e0482ac
Nicolas D'Addabbo <nico.daddabbo7100@gmail.com>
2023-06-19 19:59:05 +0200

    test

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ const ip = require('ip')
 // 	message: 'Unauthorized'
 // };
 
+const myip = ip.address();
+console.log(myip);
 appRecipe.use(cors())
 dataBase.sequelize.authenticate()
 .then(() => console.log('ok'))
@@ -37,8 +39,6 @@ if (process.env.NODE_ENV === "development") {
 appRecipe.use(express.static('public'));
 // appRecipe.use(AccessControl(options));
 appRecipe.use(express.json())
-const myip = ip.address();
-console.log(myip);
 appRecipe.use('/api', route);
 // main.sendMail().catch(console.error)
 
